9aa16e0f6a7252c3a3c1efd45df6d7f33c5cf8fa
rakesh.kumar@ph.com <ubuntu@ip-172-31-46-56.us-west-2.compute.internal>
2013-07-20 14:46:53 +0000

    index read

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!rakesh');
+var fs = require('fs');
+fs.readFileSync('index.html' , function (err, data) {
+  if (err) throw err;
+response.send(data);  
+}); 
+
+// response.send('Hello World!rakesh');
 });
 
 var port = process.env.PORT || 5000;
