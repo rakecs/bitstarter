00fb7f107a9659584a33ce8e3cce432b10913c8b
rakesh.kumar@ph.com <ubuntu@ip-172-31-46-56.us-west-2.compute.internal>
2013-07-27 13:33:49 +0000

    index final

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ response.send(data.toString());
 // response.send('Hello World!rakesh');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT ||8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
